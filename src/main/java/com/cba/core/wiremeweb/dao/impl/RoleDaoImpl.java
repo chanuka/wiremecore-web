fbb53f0ea0fc73d21edde46536c985311a3f5885
bitchanuka <chanuucsc@gmail.com>
2024-01-12 10:50:35 +0530

    - changed the service and dao classes,role mgt

--- src/main/java/com/cba/core/wiremeweb/dao/impl/RoleDaoImpl.java
+++ src/main/java/com/cba/core/wiremeweb/dao/impl/RoleDaoImpl.java
@@ -25,198 +25,80 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Component
-@Transactional
+@Repository
 @RequiredArgsConstructor
-public class RoleDaoImpl implements GenericDao<RoleResponseDto, RoleRequestDto> {
+public class RoleDaoImpl implements GenericDao<Role, Role> {
 
     private final RoleRepository repository;
-    private final GlobalAuditEntryRepository globalAuditEntryRepository;
-    private final ObjectMapper objectMapper;
-    private final UserBeanUtil userBeanUtil;
 
-    @Value("${application.resource.roles}")
-    private String resource;
 
     @Override
     @Cacheable("roles")
-    public Page<RoleResponseDto> findAll(int page, int pageSize) throws Exception {
-
+    public Page<Role> findAll(int page, int pageSize) throws Exception {
         Pageable pageable = PageRequest.of(page, pageSize);
-
-        Page<Role> entitiesPage = repository.findAll(pageable);
-        if (entitiesPage.isEmpty()) {
-            throw new NotFoundException("No Roles found");
-        }
-        return entitiesPage.map(RoleMapper::toDto);
+        return repository.findAll(pageable);
     }
 
     @Override
     @Cacheable("roles")
-    public List<RoleResponseDto> findAll() throws Exception {
-        List<Role> entityList = repository.findAll();
-        if (entityList.isEmpty()) {
-            throw new NotFoundException("No Roles found");
-        }
-        return entityList
-                .stream()
-                .map(RoleMapper::toDto)
-                .collect(Collectors.toList());
+    public List<Role> findAll() throws Exception {
+        return repository.findAll();
     }
 
     @Override
-    public Page<RoleResponseDto> findBySearchParamLike(Map<String, String> searchParamList, int page, int pageSize) throws Exception {
-
+    public Page<Role> findBySearchParamLike(Map<String, String> searchParamList, int page, int pageSize) throws Exception {
         Pageable pageable = PageRequest.of(page, pageSize);
         Specification<Role> spec = RoleSpecification.roleNameLike(searchParamList.get("roleName"));
-        Page<Role> entitiesPage = repository.findAll(spec, pageable);
+        return repository.findAll(spec, pageable);
 
-        if (entitiesPage.isEmpty()) {
-            throw new NotFoundException("No Roles found");
-        }
-        return entitiesPage.map(RoleMapper::toDto);
     }
 
     @Override
-    public Page<RoleResponseDto> findBySearchParamLikeByKeyWord(Map<String, String> searchParameter, int page, int pageSize) throws Exception {
+    public Page<Role> findBySearchParamLikeByKeyWord(Map<String, String> searchParameter, int page, int pageSize) throws Exception {
         return null;
     }
 
     @Override
-    public RoleResponseDto findById(int id) throws Exception {
-        Role entity = repository.findById(id).orElseThrow(() -> new NotFoundException("Role not found"));
-        return RoleMapper.toDto(entity);
+    public Role findById(int id) throws Exception {
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("Role not found"));
     }
 
     @Override
     @CacheEvict(value = "roles", allEntries = true)
-    public RoleResponseDto deleteById(int id) throws Exception {
-        try {
-            Role entity = repository.findById(id).orElseThrow(() -> new NotFoundException("Role not found"));
-            RoleResponseDto responseDto = RoleMapper.toDto(entity);
+    public Role deleteById(int id) throws Exception {
+        repository.deleteById(id);
 
-            repository.deleteById(id);
-            globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.DELETE.getValue(),
-                    id, objectMapper.writeValueAsString(responseDto), null,
-                    userBeanUtil.getRemoteAdr()));
-
-            return responseDto;
-
-        } catch (Exception rr) {
-            throw rr;
-        }
+        return new Role();
     }
 
     @Override
     @CacheEvict(value = "roles", allEntries = true)
     public void deleteByIdList(List<Integer> idList) throws Exception {
-        try {
-
-            idList.stream()
-                    .map((id) -> repository.findById(id).orElseThrow(() -> new NotFoundException("Role not found")))
-                    .collect(Collectors.toList());
-
-            repository.deleteAllByIdInBatch(idList);
-
-            idList.stream()
-                    .forEach(item -> {
-                        ObjectNode objectNode = objectMapper.createObjectNode();
-                        objectNode.put("id", item);
-                        try {
-                            globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.DELETE.getValue(),
-                                    item, objectMapper.writeValueAsString(objectNode), null,
-                                    userBeanUtil.getRemoteAdr()));
-                        } catch (Exception e) {
-                            throw new RuntimeException("Exception occurred for Auditing: ");// only unchecked exception can be passed
-                        }
-                    });
-        } catch (Exception ee) {
-            ee.printStackTrace();
-            throw ee;
-        }
+        repository.deleteAllByIdInBatch(idList);
     }
 
     @Override
     @CacheEvict(value = "roles", allEntries = true)
-    public RoleResponseDto updateById(int id, RoleRequestDto requestDto) throws Exception {
-
-        Role toBeUpdated = repository.findById(id).orElseThrow(() -> new NotFoundException("Role not found"));
-
-        boolean updateRequired = false;
-        Map<String, Object> oldDataMap = new HashMap<>();
-        Map<String, Object> newDataMap = new HashMap<>();
-
-        if (!toBeUpdated.getRoleName().equals(requestDto.getRoleName())) {
-            updateRequired = true;
-            oldDataMap.put("roleName", toBeUpdated.getRoleName());
-            newDataMap.put("roleName", requestDto.getRoleName());
-
-            toBeUpdated.setRoleName(requestDto.getRoleName());
-        }
-        if (!toBeUpdated.getStatus().getStatusCode().equals(requestDto.getStatus())) {
-            updateRequired = true;
-            oldDataMap.put("status", toBeUpdated.getStatus().getStatusCode());
-            newDataMap.put("status", requestDto.getStatus());
-
-            toBeUpdated.setStatus(new Status(requestDto.getStatus()));
-        }
-        if (updateRequired) {
-
-            repository.saveAndFlush(toBeUpdated);
-            globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.UPDATE.getValue(),
-                    id, objectMapper.writeValueAsString(oldDataMap), objectMapper.writeValueAsString(newDataMap),
-                    userBeanUtil.getRemoteAdr()));
-
-            return RoleMapper.toDto(toBeUpdated);
-
-        } else {
-            throw new NotFoundException("No Changes found");
-        }
+    public Role updateById(int id, Role toBeUpdated) throws Exception {
+        return repository.saveAndFlush(toBeUpdated);
     }
 
     @Override
     @CacheEvict(value = "roles", allEntries = true)
-    public RoleResponseDto create(RoleRequestDto requestDto) throws Exception {
-
-        Role toInsert = RoleMapper.toModel(requestDto);
-
-        Role savedEntity = repository.save(toInsert);
-        RoleResponseDto responseDto = RoleMapper.toDto(savedEntity);
-        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.CREATE.getValue(),
-                savedEntity.getId(), null, objectMapper.writeValueAsString(responseDto),
-                userBeanUtil.getRemoteAdr()));
-
-        return responseDto;
+    public Role create(Role toInsert) throws Exception {
+        return repository.save(toInsert);
     }
 
     @Override
     @CacheEvict(value = "roles", allEntries = true)
-    public List<RoleResponseDto> createBulk(List<RoleRequestDto> requestDtoList) throws Exception {
-
-        List<Role> entityList = requestDtoList
-                .stream()
-                .map(RoleMapper::toModel)
-                .collect(Collectors.toList());
-
-        return repository.saveAll(entityList)
-                .stream()
-                .map(item -> {
-                    RoleResponseDto responseDto = RoleMapper.toDto(item);
-                    try {
-                        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.CREATE.getValue(),
-                                item.getId(), null, objectMapper.writeValueAsString(responseDto),
-                                userBeanUtil.getRemoteAdr()));
-                    } catch (Exception e) {
-                        throw new RuntimeException("Exception occurred in Auditing: ");// only unchecked exception can be passed
-                    }
-                    return responseDto;
-                })
-                .collect(Collectors.toList());
+    public List<Role> createBulk(List<Role> entityList) throws Exception {
+        return repository.saveAll(entityList);
     }
 }
--- src/main/java/com/cba/core/wiremeweb/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cba/core/wiremeweb/service/impl/RoleServiceImpl.java
@@ -3,8 +3,19 @@ package com.cba.core.wiremeweb.service.impl;
 import com.cba.core.wiremeweb.dao.GenericDao;
 import com.cba.core.wiremeweb.dto.RoleRequestDto;
 import com.cba.core.wiremeweb.dto.RoleResponseDto;
+import com.cba.core.wiremeweb.exception.NotFoundException;
+import com.cba.core.wiremeweb.mapper.RoleMapper;
+import com.cba.core.wiremeweb.model.GlobalAuditEntry;
+import com.cba.core.wiremeweb.model.Role;
+import com.cba.core.wiremeweb.model.Status;
+import com.cba.core.wiremeweb.repository.GlobalAuditEntryRepository;
+import com.cba.core.wiremeweb.repository.specification.RoleSpecification;
 import com.cba.core.wiremeweb.service.GenericService;
 import com.cba.core.wiremeweb.util.UserBeanUtil;
+import com.cba.core.wiremeweb.util.UserOperationEnum;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -13,7 +24,11 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
@@ -22,27 +37,49 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class RoleServiceImpl implements GenericService<RoleResponseDto, RoleRequestDto> {
 
-    private final GenericDao<RoleResponseDto, RoleRequestDto> dao;
+    private final GenericDao<Role, Role> dao;
     private final UserBeanUtil userBeanUtil;
+    private final GlobalAuditEntryRepository globalAuditEntryRepository;
+    private final ObjectMapper objectMapper;
+
+    @Value("${application.resource.roles}")
+    private String resource;
 
     @Override
     public Page<RoleResponseDto> findAll(int page, int pageSize) throws Exception {
-        return dao.findAll(page, pageSize);
+        Page<Role> entitiesPage = dao.findAll(page, pageSize);
+        if (entitiesPage.isEmpty()) {
+            throw new NotFoundException("No Roles found");
+        }
+        return entitiesPage.map(RoleMapper::toDto);
     }
 
     @Override
     public List<RoleResponseDto> findAll() throws Exception {
-        return dao.findAll();
+        List<Role> entityList = dao.findAll();
+        if (entityList.isEmpty()) {
+            throw new NotFoundException("No Roles found");
+        }
+        return entityList
+                .stream()
+                .map(RoleMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Page<RoleResponseDto> findBySearchParamLike(Map<String, String> searchParamList, int page, int pageSize) throws Exception {
-        return dao.findBySearchParamLike(searchParamList, page, pageSize);
+        Page<Role> entitiesPage = dao.findBySearchParamLike(searchParamList, page, pageSize);
+        if (entitiesPage.isEmpty()) {
+            throw new NotFoundException("No Roles found");
+        }
+        return entitiesPage.map(RoleMapper::toDto);
     }
 
     @Override
@@ -52,43 +89,137 @@ public class RoleServiceImpl implements GenericService<RoleResponseDto, RoleRequ
 
     @Override
     public RoleResponseDto findById(int id) throws Exception {
-        return dao.findById(id);
+        Role entity = dao.findById(id);
+        return RoleMapper.toDto(entity);
     }
 
     @Override
     public RoleResponseDto deleteById(int id) throws Exception {
-        return dao.deleteById(id);
+        Role entity = dao.findById(id);
+        RoleResponseDto responseDto = RoleMapper.toDto(entity);
+
+        dao.deleteById(id);
+        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.DELETE.getValue(),
+                id, objectMapper.writeValueAsString(responseDto), null,
+                userBeanUtil.getRemoteAdr()));
+
+        return responseDto;
     }
 
     @Override
     public void deleteByIdList(List<Integer> idList) throws Exception {
+        idList.stream()
+                .map((id) -> {
+                    try {
+                        return dao.findById(id);
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                    return null;
+                })
+                .collect(Collectors.toList());
+
         dao.deleteByIdList(idList);
+
+        idList.stream()
+                .forEach(item -> {
+                    ObjectNode objectNode = objectMapper.createObjectNode();
+                    objectNode.put("id", item);
+                    try {
+                        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.DELETE.getValue(),
+                                item, objectMapper.writeValueAsString(objectNode), null,
+                                userBeanUtil.getRemoteAdr()));
+                    } catch (Exception e) {
+                        throw new RuntimeException("Exception occurred for Auditing: ");// only unchecked exception can be passed
+                    }
+                });
 
     }
 
     @Override
     public RoleResponseDto updateById(int id, RoleRequestDto requestDto) throws Exception {
-        return dao.updateById(id, requestDto);
+        Role toBeUpdated = dao.findById(id);
+
+        boolean updateRequired = false;
+        Map<String, Object> oldDataMap = new HashMap<>();
+        Map<String, Object> newDataMap = new HashMap<>();
+
+        if (!toBeUpdated.getRoleName().equals(requestDto.getRoleName())) {
+            updateRequired = true;
+            oldDataMap.put("roleName", toBeUpdated.getRoleName());
+            newDataMap.put("roleName", requestDto.getRoleName());
+
+            toBeUpdated.setRoleName(requestDto.getRoleName());
+        }
+        if (!toBeUpdated.getStatus().getStatusCode().equals(requestDto.getStatus())) {
+            updateRequired = true;
+            oldDataMap.put("status", toBeUpdated.getStatus().getStatusCode());
+            newDataMap.put("status", requestDto.getStatus());
+
+            toBeUpdated.setStatus(new Status(requestDto.getStatus()));
+        }
+        if (updateRequired) {
+
+            dao.updateById(id, toBeUpdated);
+            globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.UPDATE.getValue(),
+                    id, objectMapper.writeValueAsString(oldDataMap), objectMapper.writeValueAsString(newDataMap),
+                    userBeanUtil.getRemoteAdr()));
+
+            return RoleMapper.toDto(toBeUpdated);
+
+        } else {
+            throw new NotFoundException("No Changes found");
+        }
     }
 
     @Override
     public RoleResponseDto create(RoleRequestDto requestDto) throws Exception {
-        return dao.create(requestDto);
+        Role toInsert = RoleMapper.toModel(requestDto);
+
+        Role savedEntity = dao.create(toInsert);
+        RoleResponseDto responseDto = RoleMapper.toDto(savedEntity);
+        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.CREATE.getValue(),
+                savedEntity.getId(), null, objectMapper.writeValueAsString(responseDto),
+                userBeanUtil.getRemoteAdr()));
+
+        return responseDto;
     }
 
     @Override
     public List<RoleResponseDto> createBulk(List<RoleRequestDto> requestDtoList) throws Exception {
-        return dao.createBulk(requestDtoList);
+        List<Role> entityList = requestDtoList
+                .stream()
+                .map(RoleMapper::toModel)
+                .collect(Collectors.toList());
+
+        return dao.createBulk(entityList)
+                .stream()
+                .map(item -> {
+                    RoleResponseDto responseDto = RoleMapper.toDto(item);
+                    try {
+                        globalAuditEntryRepository.save(new GlobalAuditEntry(resource, UserOperationEnum.CREATE.getValue(),
+                                item.getId(), null, objectMapper.writeValueAsString(responseDto),
+                                userBeanUtil.getRemoteAdr()));
+                    } catch (Exception e) {
+                        throw new RuntimeException("Exception occurred in Auditing: ");// only unchecked exception can be passed
+                    }
+                    return responseDto;
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public byte[] exportPdfReport() throws Exception {
 
-        List<RoleResponseDto> ResponseDtoList = dao.findAll();
+        List<RoleResponseDto> responseDtoList = dao.findAll()
+                .stream()
+                .map(RoleMapper::toDto)
+                .collect(Collectors.toList()
+                );
         //load file and compile it
         File file = ResourceUtils.getFile("classpath:report/role.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(ResponseDtoList);
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(responseDtoList);
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "Created By :" + userBeanUtil.getUsername()); // username can be extracted once the url is accessible
 
@@ -109,8 +240,11 @@ public class RoleServiceImpl implements GenericService<RoleResponseDto, RoleRequ
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(columnHeaders[i]);
         }
-        List<RoleResponseDto> responseDtoList = dao.findAll();
-
+        List<RoleResponseDto> responseDtoList = dao.findAll()
+                .stream()
+                .map(RoleMapper::toDto)
+                .collect(Collectors.toList()
+                );
         int rowCount = 1;
 
         for (RoleResponseDto responseDto : responseDtoList) {
